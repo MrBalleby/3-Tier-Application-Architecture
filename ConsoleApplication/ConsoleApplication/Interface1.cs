dfcd68b5665244b5f4398dcaf7bf8d87cb44eea9
Jan <jhni.skp@edu.mercantec.dk>
2019-10-03 08:40:05 +0200

    First interface

---
+++ ConsoleApplication/ConsoleApplication/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    interface IConcertHall
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string ticketType { get; set; }
+        public Nullable<int> totalOfPpl { get; set; }
+        public int AddressId { get; set; }
+    }
+}
+
