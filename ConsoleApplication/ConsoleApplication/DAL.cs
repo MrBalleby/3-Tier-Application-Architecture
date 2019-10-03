abf441aff6119eedead3efba4b283cfc7c9bd26f
Mark <mark.ballebytech.dev@gmail.com>
2019-10-03 08:27:11 +0200

    Created DAL Layer & Added Entity Data Model as reference

---
+++ ConsoleApplication/ConsoleApplication/DAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class DAL
+    {
+        private string conn = ConfigurationManager.ConnectionStrings["CON"].ToString();
+
+        public void InsertUpdateDeleteSQLString(string sqlstring)
+        {
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sqlstring, con);
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public object ExecuteSqlString(string sqlstring)
+        {
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand(sqlstring, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
